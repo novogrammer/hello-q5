10930e9757066dbc0cda7d68baff479d944c73b9
Yusuke Kawamoto <novogrammer@gmail.com>
2026-03-05 17:21:41 +0900

    q5導入

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
 import './style.css'
 
-console.log("main.ts");
+import "q5";
+
+
+
+/**
+ * Made with q5!
+ * https://q5js.org
+ */
+
+await Canvas();
+background(color('silver'));
+
+q5.draw = function () {
+  circle(mouseX, mouseY, random(55));
+}
